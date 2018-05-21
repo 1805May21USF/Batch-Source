22f9c20cac24ad56e4bbd7f668d64cca8f3c63ad
Tiffany <tiffanyttran28@gmail.com>
2018-05-21 16:12:41 -0400

    Adding Week 1 Day 1 Project

---
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,10 @@
+package com.revature.driver;
+
+//Packages  - namespace that organizes a set of related classes and interfaces
+public class FunStuff {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
