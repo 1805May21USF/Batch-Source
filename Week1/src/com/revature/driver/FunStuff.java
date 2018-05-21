f2968f1f2f1df3de76324e4124aacbbb370e46de
Nicholas Low <n.low2492@gmail.com>
2018-05-21 16:44:27 -0400

    Replacing stuff

---
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,23 @@
+package com.revature.driver;
+
+public class FunStuff {
+/*
+ * Commnts!
+ * 
+ * Naming Conventions:
+ * Classes + Projects: Pascal Case capitialize each word
+ * Methods and Variables: Camel Case ex. firstSecondThird
+ * Package Names: lowercase separated by periods
+ * Constants: ALLCAPS
+ */
+	
+//main method is the entry point
+	public static void main(String[] args) {
+		/*
+		 * static don't need an instance/belong to class
+		 * void: doesn't return anything
+		 * String [] args: args to be utilized in method
+		 */
+		System.out.println("I did a thing!");
+	}
+}
