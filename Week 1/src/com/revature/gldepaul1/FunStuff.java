3c8ec1d539d91bd21a40a50edddd18b0797b33ac
Gavin DePaul <gavindepaul1@gmail.com>
2018-05-21 13:20:06 -0700

    This is my Java fil containing notes from May 21st

---
+++ Week 1/src/com/revature/gldepaul1/FunStuff.java
@@ -0,0 +1,41 @@
+package com.revature.gldepaul1;
+
+//Packages-Namespace that organizaes
+//a set of related classes and interfaces
+
+
+
+
+public class FunStuff {
+	/* Another form of comments.
+	 * That spans multiple lines
+	 * 
+	 * Naming conventions
+	 * Classes and Projects: Pascal Case 
+	 * capitalize each word
+	 * methods and variables: Camel Case
+	 * firstSecondThird
+	 * Package names: lowercase, seperated by periods
+	 * constants: ALLCAPSLOLZ
+	 * 
+	 */
+	
+//main method is the entry point
+	public static void main(String args) {
+		/*
+		 * Public - Anything that can access the file
+		 * can acess this method
+		 * 
+		 * Static - don't need an instance to run iy 
+		 * and the method belongs to a class
+		 * 
+		 * Void: doesn't return anything
+		 * 
+		 * String[]args: arguments to be utilized in method
+		 * 
+		 */
+	
+		System.out.println("I did a thing!");
+	}
+	
+}
