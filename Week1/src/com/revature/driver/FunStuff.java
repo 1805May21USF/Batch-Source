855b6a869e8bba366b58f8c1c6cb6a928408c8d0
Nathaniel Simpson <nathaniel.e.simpson1114@gmail.com>
2018-05-21 16:12:24 -0400

    Adding Week1 fun stuff

---
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,25 @@
+package com.revature.driver;
+
+//Packages-Namespace that organizes a set of related classes and interfaces
+public class FunStuff {
+	/*
+	 * Comments!
+	 * 
+	 * Naming Conventions
+	 * Classes and Projects: PascalCase
+	 * Methods and Variables: camelCase
+	 * Package names: lowercase, separated by periods
+	 * Constants: ALLCAPS
+	 */
+	
+	//main method is the entry point
+	public static void main(String[] args) {
+		/*
+		 * static: don't need an instance to run it/belongs to a class
+		 * void: doesn't return anything 
+		 * String[] args: arguments to be utilized in method
+		 */
+		System.out.println("Gaming + anime = life <3");
+	}
+	
+}
