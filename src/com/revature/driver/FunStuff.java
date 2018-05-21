b174c6ebdbc6150060cd86c4d7510751d3478734
rafaeltx <rafb.mariano@gmail.com>
2018-05-21 16:12:23 -0400

    Adding week1 project

---
+++ src/com/revature/driver/FunStuff.java
@@ -0,0 +1,27 @@
+package com.revature.driver;
+
+//Packages - Name space that organizes a set of related classes and interfaces
+public class FunStuff {
+/*
+ * 
+ * Naming Conventions
+ * Classes and Projects: Pascal Case (Capitalize each work)
+ * methods and variables: Camel Case (firstSecondThird)
+ * Package names: lowercase, seperated by periods
+ * constants: ALLCAPS
+ * 
+ * 
+ */
+	
+//main method is the entry point
+	public static void main(String[] args) 
+	{
+		//static doesn't need an instance/ it belongs to a class
+		//void: doesn't return anything
+		//String[] args: arguments to be utilized in method
+		System.out.println("I did a thing!"); 
+		
+	}
+	
+	
+}
