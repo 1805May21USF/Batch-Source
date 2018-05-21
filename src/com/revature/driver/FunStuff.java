06854d550bf18a5fa0b2860b14a27f456fbe5489
mmt200 <michaelatate21@gmail.com>
2018-05-21 16:27:14 -0400

    added FunStuff.java and file heirarchy

---
+++ src/com/revature/driver/FunStuff.java
@@ -0,0 +1,37 @@
+package com.revature.driver;
+//Packages-Namespace that organizes 
+//a set of related classes and interfaces.
+
+public class FunStuff {
+	/*
+	 * 
+	 * Comments!
+	 * 
+	 * 
+	 * Naming Conventions
+	 * Classes and Projects: Pascal Case capitalize each word
+	 * methods and variables: Camel Case ex: firstSecondThird
+	 * Package names: lowercase, seperated by periods
+	 * constants: ALLCAPS
+	 *
+	 */
+	
+	
+	
+	
+	
+	
+	//Main Method is the entry point
+	public static void main(String[] args) {
+		
+		
+		/*
+		 * static don't need an instance / belongs to the class
+		 * void- no return value
+		 * String [] args : arguments to be utilized in method
+		 */
+		// TODO Auto-generated method stub
+		System.out.println("Hi");
+	}
+
+}
