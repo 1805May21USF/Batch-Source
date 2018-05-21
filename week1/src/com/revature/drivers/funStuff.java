937f5e32a0e15f49fcf9562c0bcf6a6d508ea384
Kesiride Michael Nwokocha <kesiride@gmail.com>
2018-05-21 13:15:22 -0700

    week one lesson

---
+++ week1/src/com/revature/drivers/funStuff.java
@@ -0,0 +1,25 @@
+package com.revature.drivers;
+
+/*Packages-Namespaces that organises a set of related classes and inheritances*/
+public class funStuff {
+	/*
+	 * Comments!
+	 * 
+	 * Naming Conventions:
+	 * Classes and Projects!]: Pascal Case - capitalize each word
+	 * Methods and Variables: Camel Case - e.g firstSecondThird
+	 * Packages: lowercase separated by periods.
+	 * Constants: ALLCAPS
+	 */
+	
+	//main ethod is the entry point
+	public static void main(String[]args) {
+		/*
+		 * static: dont need an instance. Belongs to class
+		 * void: doesnt return anything
+		 * String[]args: arguments passed through the command line to be utilized by the method.
+		 */ 
+		System.out.println("Eko oni baje oo");
+	}
+	
+}
