1c07a638ecae0fcb5c921d0c8c449dadde926b29
Shuva Chowdhury <spc2692@gmail.com>
2018-05-21 16:12:40 -0400

    Project Wk_One is added

---
+++ Wk_One/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,27 @@
+package com.revature.driver;
+
+//Packages-Namespace that organizes
+//a set of related classes and interfaces
+
+public class FunStuff {
+/*
+ * Comments!
+ * 
+ * Naming Conventions
+ * Classes and Projects: Pascal Case capitalize each word
+ * methods and variables: Camel Case
+ * Package names: lowercase, separated by periods
+ * constants: ALLCAPS
+ */
+	
+	//main method is the entry point
+	public static void main(String[] args) {
+		/*
+		 * static: don't need an instance/ belongs to class
+		 * void: doesn't return anything
+		 * String [] args: args to be utilized in method
+		 */
+		System.out.println("Revature Rocks!!!");
+	}
+
+}
