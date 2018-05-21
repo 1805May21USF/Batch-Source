547d6e4d24902079c25cb09a3817b451ec21c467
Scott Blasing <sdblasing@gmail.com>
2018-05-21 16:24:45 -0400

    Week 1 Test

---
+++ src/com/revature/driver/FunStuff.java
@@ -0,0 +1,21 @@
+package com.revature.driver;
+/*Packages-Namespace that organizes
+* 
+*a set of related classes and interfaces
+*/
+
+public class FunStuff {
+	/*
+	 * Comments!
+	 * 
+	 * Naming Conventions
+	 * Classes and Projects: Pascal Case capitalize each word
+	 * methods and variables: Camal Case ex. firstSecondThird
+	 * Package names: lowercase, seperated by periods
+	 * constants: ALLCAPSLOLZ
+	 */
+	
+	public static void main(String[] args) {
+		System.out.println("I do things");
+		}
+}
