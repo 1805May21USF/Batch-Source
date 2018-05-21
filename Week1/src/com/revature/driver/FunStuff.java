bc0244dc4b2e1114260d5e33890d1aec6a736fb6
Derek Moore <ddmoore1221@gmail.com>
2018-05-21 16:12:27 -0400

    Adding Week1 project

---
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,13 @@
+package com.revature.driver;
+
+public class FunStuff {
+	
+	//main method is the entry point
+	//static: don't need an instance / belongs to class
+	//void: doesn't return anything
+	//String[] args: arguments to be utilized in method
+	public static void main(String[] args) {
+		System.out.println("Hello");
+	}
+
+}
