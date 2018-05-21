27c2bf006526692dc96ca9ae42c54a54b832ea80
LAPTOP-H48F48M9\JonWi <jwisnie5@kent.edu>
2018-05-21 16:18:08 -0400

    Week One: Test Project

---
+++ Week One/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,28 @@
+package com.revature.driver;
+
+/*
+ * Packages-Namespace that organizes
+ * a set of related classes and interfaces
+ */
+
+public class FunStuff {
+
+	/*
+	 * Naming Conversions
+	 * Classes and Projects: Pascel Case (Capitalize Each Work)
+	 * Methods and Variables: Camel Case (lowercase first word and capitalize every word after)
+	 * Package Names: lowercase, separated by periods
+	 * Constants: All Capitals
+	 */
+	
+	//Main method is the entry point
+	
+	public static void main(String[] args) {
+		/*
+		 * static does not need an instance to belong to a class
+		 * void: Does not return anything
+		 * String[] args: Args to be utilized in a method
+		 */
+		System.out.println("Hello World");
+	}
+}
