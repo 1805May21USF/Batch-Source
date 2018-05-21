001fa195dd3f6b3a3a6c6b8ce4fccbad42615542
bryanfritz1978 <bryanfritz1978@gmail.com>
2018-05-21 16:12:51 -0400

    added first project Week1

---
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -0,0 +1,23 @@
+package com.revature.driver;
+
+public class FunStuff {
+			/*
+			 * Naming Convention 
+			 * Classes and Projects: Pascal Case capital first letter of each word
+			 * methods and variables: Camel Case lower firstword then capital letter of the rest
+			 * Package name: lower case separated by periods
+			 * Constants: ALLCAPS
+			 * 
+			 * Static: does not need an instance belongs to the class
+			 * void: does not return anything
+			 * String[] args: args to be utilized in the mothod from the command line
+			 * 
+			 * 
+			 */
+	
+	public static void main(String[] args) {
+		System.out.println("Hi");
+
+	}
+
+}
