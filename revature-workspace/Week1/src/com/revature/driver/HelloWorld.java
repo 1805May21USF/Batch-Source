c8df2fa774c4ef2f547a89dda7ac5f3b4fc09979
aderao01 <gboyegaaderele@gmail.com>
2018-05-21 16:30:15 -0400

    Hello World practice

---
+++ revature-workspace/Week1/src/com/revature/driver/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.revature.driver;
+
+public class HelloWorld {
+
+		
+		/*
+		 *  Comments!
+		 *  
+		 *  Naming Conventions
+		 *  Classes and Projects: Pascal case capitalize each word
+		 *  Methods and Variable: Camel Case: ex. firstSecondThird
+		 *  Package names: lowercase, separated by periods 
+		 *  constants: ALLCAPS
+		 */
+	
+		//main method is the entry point 
+		public static void main(String[] args )
+		{
+			System.out.println("Woo");
+		}
+
+}
