3a2f901fdbcc0ce399efadbe59d8e2f8faba7e25
bryanfritz1978 <bryanfritz1978@gmail.com>
2018-05-22 09:36:17 -0400

    another one

--- Week1/src/com/revature/driver/FunStuff.java
+++ Week1/src/com/revature/driver/FunStuff.java
@@ -1,27 +1,5 @@
 package com.revature.driver;
 
-<<<<<<< HEAD
-public class FunStuff {
-			/*
-			 * Naming Convention 
-			 * Classes and Projects: Pascal Case capital first letter of each word
-			 * methods and variables: Camel Case lower firstword then capital letter of the rest
-			 * Package name: lower case separated by periods
-			 * Constants: ALLCAPS
-			 * 
-			 * Static: does not need an instance belongs to the class
-			 * void: does not return anything
-			 * String[] args: args to be utilized in the mothod from the command line
-			 * 
-			 * 
-			 */
-	
-	public static void main(String[] args) {
-		System.out.println("Hi");
-
-	}
-
-=======
 //Packages-Namespace that organizes 
 //a set of related classes and interfaces
 
@@ -35,7 +13,19 @@ public class FunStuff {
  * Package names: lowercase, seperated by periods
  * constants: ALLCAPSLOLZ
  */
-
+	/*
+	 * Naming Convention 
+	 * Classes and Projects: Pascal Case capital first letter of each word
+	 * methods and variables: Camel Case lower firstword then capital letter of the rest
+	 * Package name: lower case separated by periods
+	 * Constants: ALLCAPS
+	 * 
+	 * Static: does not need an instance belongs to the class
+	 * void: does not return anything
+	 * String[] args: args to be utilized in the mothod from the command line
+	 * 
+	 * 
+	 */
 //main method is the entry point
 	public static void main(String[] args) {
 		/*
@@ -45,5 +35,5 @@ public class FunStuff {
 		 */
 		System.out.println("I did a thing!");
 	}
->>>>>>> 3d37a58c25fbc39281d0fb06edfc9199336a10fc
+
 }
