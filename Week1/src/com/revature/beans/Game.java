9155b399a0324eeec2e3a6524ab42844d53f62f5
Tiffany <tiffanyttran28@gmail.com>
2018-05-22 14:15:42 -0400

    Updating files

--- Week1/src/com/revature/beans/Game.java
+++ Week1/src/com/revature/beans/Game.java
@@ -1,7 +1,6 @@
 package com.revature.beans;
 
 public class Game {
-<<<<<<< HEAD
 	
 	//Static code block
 	static {
@@ -19,8 +18,6 @@ public class Game {
 		 */
 		System.out.println("Inside the code block");
 	}
-	
-=======
 	//Static code block
 	static {
 		//Static code blocks are similar.
@@ -35,7 +32,6 @@ public class Game {
 		//yet.
 		System.out.println("Inside code block");
 	}
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 	private String name;
 	private String genre;
 	private double price;
@@ -43,44 +39,20 @@ public class Game {
 	//overloading constructors
 	public Game() {
 		this.setName("Tetris");
-<<<<<<< HEAD
 		this.setGenre("Good");
 		this.setPrice(1.50);
 		System.out.println("This is tetris.");
-	}
-		
-=======
-		this.setGenre("Crap");
-		this.setPrice(1.58);
-		System.out.println("I created a crap game");
+
 	}
 	
-	
-	
-	
-	
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 	public Game(String name, String genre, double price) {
 		super();
 		this.name = name;
 		this.genre = genre;
 		this.price = price;
-<<<<<<< HEAD
 		Person s = new Person("Jim");
 		System.out.println(s.getName() + " is playing a game");
 	}
-
-
-	public String getName() {
-		return name;
-	}
-=======
-		Person s= new Person("Jim");
-		System.out.println("I created a game and a person named "+
-		s.getName());
-	}
-
-
 
 
 
@@ -88,7 +60,6 @@ public class Game {
 		return name;
 	}
 	
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -104,14 +75,10 @@ public class Game {
 	public void setPrice(double price) {
 		this.price = price;
 	}
-<<<<<<< HEAD
-	
 
-=======
 	@Override
 	public String toString() {
 		return "Game [name=" + name + ", genre=" + genre + ", price=" + price + "]";
 	}
 	
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 }
--- Week1/src/com/revature/beans/Person.java
+++ Week1/src/com/revature/beans/Person.java
@@ -1,29 +1,6 @@
 package com.revature.beans;
 
 public class Person {
-<<<<<<< HEAD
-	
-	//Encapsulation
-	private String name;
-	private int age;
-	private int weight;
-	private static String homeplanet = "Earth";
-	
-	//Constructors
-	public Person() {
-		super();
-		
-	}
-	public Person(String name) {
-		this(name, 20);
-	}
-	
-	public Person(String name, int age) {
-		this(name, age, 200);
-	}
-	
-	public Person(String name, int age, int weight) {
-=======
 	//encapsulation
 	private static String homePlanet="earth"; 
 	private String name;
@@ -47,24 +24,15 @@ public class Person {
 	
 	public Person(String name, int age, int weight) {
 		super();
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 		this.name = name;
 		this.age = age;
 		this.weight = weight;
 	}
-<<<<<<< HEAD
-
-=======
 	
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 	public String getName() {
 		return name;
 	}
 	
-<<<<<<< HEAD
-=======
-
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -80,20 +48,15 @@ public class Person {
 	public void setWeight(int weight) {
 		this.weight = weight;
 	}
-<<<<<<< HEAD
-	
-	public static String getHomeplanet() {
-		return homeplanet;
-	}
-	public static void setHomeplanet(String homeplanet) {
-		Person.homeplanet = homeplanet;
+
+	public static void setHomePlanet(String homePlanet) {
+		Person.homePlanet = homePlanet;
 	}
 	@Override
 	public String toString() {
 		return "Person [name=" + name + ", age=" + age + ", weight=" + weight + "]";
 	}
 
-=======
 	public static String getHomePlanet() {
 		return homePlanet;
 	}
@@ -103,7 +66,4 @@ public class Person {
 		return this.getWeight();
 	}
 	
-	
-	
->>>>>>> d85e5520c17cd9adf752007f2227babea7b85d0f
 }
