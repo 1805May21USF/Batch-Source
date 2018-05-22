f6b789fd84b55a4cbd974208e1565a27f2b895b0
sunnarap <sunnaraprasavath@gmail.com>
2018-05-22 13:39:22 -0400

    Week 1 Updates

---
+++ Week1-Sunny/src/com/revature/beans/Man.java
@@ -0,0 +1,14 @@
+package com.revature.beans;
+
+public class Man extends Person{
+	
+	public int eat() {
+		this.setWeight(getWeight() + 10);
+		return this.getWeight();
+	}
+	
+	/*public Integer eat() {
+		this.setWeight(getWeight() + 10);
+		return this.getWeight();
+	}*/
+}
