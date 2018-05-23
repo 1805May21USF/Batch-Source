c0670606acc0e6db59ec879e965c12e9a15e7f21
Shuva Chowdhury <spc2692@gmail.com>
2018-05-23 17:03:58 -0400

    added package classtypes

---
+++ Wk_One/src/com/revature/classtypes/Driver.java
@@ -0,0 +1,13 @@
+package com.revature.classtypes;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Shark shark = new Shark();
+		System.out.println("Hi, I am a shark");
+		shark.breathe();
+		shark.findPrey();
+	}
+
+}
