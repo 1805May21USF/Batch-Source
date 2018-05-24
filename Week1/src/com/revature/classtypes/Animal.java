0c0c799f4ca8d5355ed07ce4cd70839cef7c3b01
Tiffany <tiffanyttran28@gmail.com>
2018-05-24 09:05:04 -0400

    Fixed animal

--- Week1/src/com/revature/classtypes/Animal.java
+++ Week1/src/com/revature/classtypes/Animal.java
@@ -1,23 +1,22 @@
 package com.revature.classtypes;
 
-<<<<<<< HEAD
 public class Animal {
 
 	public static void main(String[] args) {
 		cat(1.0);
 
 	}
+
 	static void cat(int i) {
 		System.out.println("Integer");
 	}
+
 	static void cat(double i) {
 		System.out.println("Double");
 	}
 
-=======
 public abstract class Animal {
 	
 	public abstract void breathe();
 	
->>>>>>> 51758b88e2983a08995e0ccd8d01b4e4fe0b58d9
 }
