9b6d461fc52bec9bd2dcd366175e052e055f4964
Tiffany <tiffanyttran28@gmail.com>
2018-05-25 13:41:03 -0400

    Added thread files

--- Week1/src/com/revature/threads/ProducerConsumerMain.java
+++ Week1/src/com/revature/threads/ProducerConsumerMain.java
@@ -1,24 +1,23 @@
 package com.revature.threads;
+
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
-
-
 
 public class ProducerConsumerMain {
 
 	public static void main(String[] args) {
-		
+
 		BlockingQueue<Message> queue = new ArrayBlockingQueue<Message>(10);
 		Producer producer = new Producer(queue);
 		Consumer consumer = new Consumer(queue, "Consumer 1");
 		Consumer consumer2 = new Consumer(queue, "Consumer 2");
-		
+
 		new Thread(producer).start();
 		Thread t = new Thread(consumer);
 		t.setPriority(10);
 		t.start();
 		new Thread(consumer2).start();
-		
+
 		System.out.println("Producer and consumers have been started...");
 
 	}
