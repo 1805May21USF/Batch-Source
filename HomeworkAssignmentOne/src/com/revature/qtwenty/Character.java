f21db28e6add851d18a80f4dd76b0faf703cafef
corwingithub <corwin.lester.stuff@gmail.com>
2018-05-26 07:23:54 -0400

    removed bad questionsixteen project

---
+++ HomeworkAssignmentOne/src/com/revature/qtwenty/Character.java
@@ -0,0 +1,11 @@
+package com.revature.qtwenty;
+
+import java.io.Serializable;
+
+public class Character implements Serializable{
+	
+	public String name;
+	public int age;
+	public String hometown;
+
+}
