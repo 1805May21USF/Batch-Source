744c832253d90d0a3020d35297004ea7a0c47ebd
corwingithub <corwin.lester.stuff@gmail.com>
2018-06-04 16:14:44 -0400

    added some unit tests

--- Corwin Week 2/JDBCBank/src/test/java/com/revature/test/TestMain.java
+++ Corwin Week 2/JDBCBank/src/test/java/com/revature/test/TestMain.java
@@ -1,5 +1,33 @@
 package com.revature.test;
 
-public class TestMain {
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+import com.revature.beans.Customer;
+import com.revature.beans.Employee;
+import com.revature.util.Datastore;
+
+public class TestMain {
+	public Datastore d;
+	@Test
+	void userTest() {
+		d = d.getInstance();
+		d.addCustomer(new Customer("test","test","test","test"));
+		Customer v = d.getCustomerByUsername("test");
+		assertTrue(v.getUserName().equals("test"));
+		d.deleteCustomer(d.getCustomerByUsername("test"));
+		assertNull(d.getCustomerByUsername("test"));
+	}
+	
+	void employeeTest() {
+		d = d.getInstance();
+		d.addEmployee(new Employee("test","test","test","test","N"));
+		Employee v = d.getEmployeeByUsername("test");
+		assertTrue(v.getUserName().equals("test"));
+		d.deleteEmployee(d.getEmployeeByUsername("test"));
+		assertNull(d.getEmployeeByUsername("test"));
+	}
 }
