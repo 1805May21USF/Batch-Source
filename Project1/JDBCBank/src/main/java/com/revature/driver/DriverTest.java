ac0e1a50031e56484d16ceb2fa742e1d3bb44f62
dustin19 <39474311+dustin19@users.noreply.github.com>
2018-06-04 16:33:02 -0400

    Update DriverTest.java

--- Project1/JDBCBank/src/main/java/com/revature/driver/DriverTest.java
+++ Project1/JDBCBank/src/main/java/com/revature/driver/DriverTest.java
@@ -34,8 +34,6 @@ public class DriverTest {
 		case 2 :
 			menuNewUser();
 			break;
-		case 3 :
-			menuSuper();
 		default:
 			System.out.println("Please enter a valid number");
 			menu1();
@@ -188,113 +186,7 @@ public class DriverTest {
 				break;
 		}
 	}
-	public static void superLogin() {
-		System.out.println("Enter user name");
-		String user = sc.next();
-		System.out.println("Enter Password");
-		String pass = sc.next();
-		
-		menuSuper();
-		
-	}
-	public static void menuSuper() {
-		UserDAOImple udi = new UserDAOImple();
-		AccountDAOImple adi = new AccountDAOImple();
-		List<Account> accounts = new ArrayList<Account>();
-		List<User> users = new ArrayList<User>();
-		try {
-			users = udi.getUserList();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		System.out.println("\n1.View all users\n"
-				+ "2.Create User\n"
-				+ "3.Update user account\n"
-				+ "4.Delete user and all account");
-		int input = sc.nextInt();
-		switch(input) {
-		case 1 :
-			System.out.println(users);
-			menuSuper();
-			break;
-		case 2 :
-			System.out.println("Enter your new username");
-			String username = sc.next();
-			System.out.println("Enter your password");
-			String password = sc.next();
-			try {
-				if(!udi.isUserName(username)) {
-					udi.createUser(username, password);
-					System.out.println("User Created");
-					menuSuper();
-				}else {
-					System.out.println("Username is taken");
-					menuSuper();
-				}
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}	
-			break;
-		case 3 :
-			System.out.println(users);
-			System.out.println("Enter the userID of the user you would like to update");
-			int temp = sc.nextInt();
-			try {
-				accounts = adi.getUserAccountsList(temp);
-				System.out.println("Select the account id you would like to update");
-				System.out.println(accounts);
-				int accountID = sc.nextInt();
-				menuUpdateAcc(accountID,accounts);
-				
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			break;
-			
-		case 4 :
-			
-			break;
-		default :
-			System.out.println("please enter a valid selection");
-		}
-	}
-	public static void menuUpdateAcc(int accountID, List<Account> accounts2) {
-		AccountDAOImple adi = new AccountDAOImple();
-		List<Account> accounts = new ArrayList<Account>();
-		for(Account a:accounts2) {
-			if(a.getAccountID()==accountID) {
-				System.out.println("what would you like to do?\n1.deposit\n2.Withdraw");
-				int input = sc.nextInt();
-				if(input == 1) {
-					System.out.println("enter amount");
-					float deposit = sc.nextFloat();
-					try {
-						adi.deposit(accountID, deposit);
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				} else if (input==2) {
-					System.out.println("enter amount");
-					float withdraw = sc.nextFloat();
-					try {
-						adi.withdraw(accountID, withdraw);
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}else {
-					System.out.println("Please enter a valid option");
-					menuUpdateAcc(accountID,accounts2);
-				}
-			}
-	}
 	
-
+	
 	}
-	}
-
 
