af02b6ab6ce2007c9efa7daab457ba1dbd6b5a35
Jason Knighten <jmknighten9@gmail.com>
2018-06-05 19:36:57 -0400

    wrap up week 3 day 2

---
+++ Week 3/example.js
@@ -0,0 +1,60 @@
+//alert("Roll Tide");
+ var a =10;
+ var b,c,d,e,f,g,h,e;
+ //Loosely Type
+ b="10";
+ c=true;
+ d= {};
+ e=null;
+ //f= ;
+g=(0/0);
+h=[];
+i=function(){};
+
+function incrementA(){
+    a+=1;
+    return a;
+}
+//Type Coercion
+//== performs tye coercion
+//=== does not
+//console.log("hi");
+//console.log("5==5");
+//console.log(5==5);
+//console.log(5===5);
+//console.log("weird");
+//console.log(5=="5");
+//console.log(5==="5");
+//console.log(false==1);
+//console.log(false==0);
+//console.log(false===0);
+//console.log(true==1000000);
+//console.log(false=="0");
+//console.log('7'+7+7);
+//console.log(7+7+'7');
+
+//Objects
+// var person={"name": "fred",
+// "age": 87};
+// person.gender= "undefined";
+// //Constructor
+// function Person (name,age){
+//     this.name =name;
+//     this.age=age;
+// }
+// //how to use constructor
+// var matt = new Person("matt",32);
+// //Different constructor type thing
+// function MakePerson(name,age){
+//     var p= {"name":name,
+// "age":age};
+//     return p;
+// }
+//Arrays
+//var arr=[10,20,30];
+//arr[9]=5;
+
+
+
+
+
